f387fc16a4fe7bcc291fc5bbb7f5a8f7d89305a0
Raj Sarkar <raj.sarkar240706@gmail.com>
2026-01-11 22:46:47 +0530

    A update regarding Render deployment issue

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express=require("express");
 const app=express(); 
 const mongoose=require("mongoose"); 
 mongoose.set("strictPopulate", false);
-const port=8080;  
+const port=process.env.PORT||8080;  
 const dburl=process.env.ATLASDB_URL;
 
 const path=require("path"); 
